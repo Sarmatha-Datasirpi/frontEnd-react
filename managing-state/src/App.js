541b7d4779a4f4a64d83e915ebb60f364628ad24
Sarmatha-Datasirpi <sarmatha.k@datasirpi.com>
2023-09-25 13:42:08 +0530

    React Task Completed

---
+++ managing-state/src/App.js
@@ -0,0 +1,14 @@
+import AddTask from './AddTask.js';
+import TaskList from './TaskList.js';
+import { TasksProvider } from './TasksContext.js';
+
+export default function TaskApp() {
+  return (
+    <TasksProvider>
+      <h1>Zara's Variety Foods</h1>
+      <AddTask />
+      <TaskList />   
+    </TasksProvider>
+
+  );
+}
